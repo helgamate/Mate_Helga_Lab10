31716a4228f23168041682138e41fafe3ec74cd7
helgamate <helga.mate@stud.ubbcluj.ro>
2022-12-20 20:13:31 +0200

    Add project files.

---
+++ gRPCClientok/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gRPCClientok.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
